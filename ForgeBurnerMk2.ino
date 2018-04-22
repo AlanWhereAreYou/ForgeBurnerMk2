ac932b5da440c64e633ad3645468f953f7dd1b05
AlanWhereAreYou <alanwhereareyou@gmail.com>
2018-04-22 19:45:01 -0400

    Most functions working
    Pot value is being read and displayed.
    Switched to using two screens for each value: One for the label, one for the value. Gave up on using a prefix.
    
    Need to add code to make the pressure transmitter re-zero, and make it display the delta. Need to also test with an actual fan.

--- ForgeBurnerMk2.ino
+++ ForgeBurnerMk2.ino
@@ -47,17 +47,20 @@
 #include <Adafruit_BMP280.h> // I2C for Pressure Sensor (PT)
 
 // Pins - Fan
-const int Pin_PWMFan = 9; // Arduino 9, this is D9/PB1/OC1A
-const int Pin_Pot = A1; // Arduino A1, PC1/ADC1
-const int Pin_Tach = 2; // Arduino 2, INT0/PD2
+const int pin_PWMFan = 9; // Arduino 9, this is D9/PB1/OC1A
+const int pin_Pot = A1; // Arduino A1, PC1/ADC1
+const int pin_Tach = 2; // Arduino 2, INT0/PD2
 
 // Pins - Display
-#define Pin_CLK 6
-#define Pin_DIO 5
+#define pin_CLK 6
+#define pin_DIO 5
 
 // BMP280 Pressure Transmitter (PT) Definitions
-Adafruit_BMP280 bme; // I2C
-const int PtAddress = 0x76;
+Adafruit_BMP280 Pt_Bme; // I2C
+const int Pt_Address = 0x76;
+float Pt_Hpa = 0.0;
+float Pt_ZeroHPa = 0.0;
+float Pt_TempC = 0.0;
 
 // Fan Definitions
 // On NMB Fan: Red = +12VDC, Black = GND, Brown = PWM, White = Tach
@@ -69,215 +72,308 @@ struct TachFeedbackData{
   unsigned long micros_Prev;
   unsigned long micros_Current;
 };
-TachFeedbackData TachData;
-#define millis_TachCaptureInterval 500; // How frequently do we want to calculate the rpm.
-volatile int pulsesCountedByInterrupt = 0;
-volatile long microsCapturedByInterrupt = 0;
-volatile bool interrupt_Lock;
-int PotIn = 0;
+TachFeedbackData fan_TachData;
+#define fan_millis_TachCaptureInterval 500; // How frequently do we want to calculate the rpm.
+volatile int fan_pulsesCountedByInterrupt = 0;
+volatile long fan_microsCapturedByInterrupt = 0;
+volatile bool fan_interrupt_Lock;
+int fan_potInRaw = 0;
+int fan_SetpointIntPct = 0;
+const int fan_SpeedChangeRawDelta = 10;
+const long fan_millisSpeedChangeFlagWindow = 5000;
 
 // Display Definitions
 #define segA 0b01110111
 #define segP 0b01110011
 #define segS 0b01101101
 #define segC 0b00111001
-TM1637_6D tm1637_6D(Pin_CLK,Pin_DIO);
+#define segTest 0b11111111
+
+TM1637_6D tm1637_6D(pin_CLK,pin_DIO);
 
 // Screen Control
-static int screenCurrent = -1;
-static long millisChangeScreenTime = millis();
-int screenTopIndex = 4;
-const long millisScreenToggleRate = 2000;
+#define screen_SetpointLabel 0
+#define screen_Setpoint 1
+#define screen_ActualLabel 2
+#define screen_Actual 3
+#define screen_RelativePressureLabel 4
+#define screen_RelativePressure 5
+#define screen_TemperatureLabel 6
+#define screen_Temperature 7
+#define screen_PressureZeroingLabel 8
+#define screen_PressureZeroing 9
+static int screen_Current = -1;
+static long screen_millisChangeScreenTime = millis();
+int screen_TopIndex = 9;
+const long screen_millisToggleRate = 2000;
 
 void setup()
 {
   Serial.begin(9600);
-  SetupDisplay;
-  SetupPT;
-  SetupFan;
-}
-
-void SetupDisplay()
-{
-  tm1637_6D.init();
-  tm1637_6D.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-  // tm1637_6D.displayFloat(0.0);  
-}
-
-void SetupPT()
-{
-  bme.begin(PtAddress);
-}
-
-void SetupFan()
-{
-  Timer1.initialize(40);  // 40 us = 25 kHz
-
-  // Attach the tach interrupt
-  pinMode(Pin_Tach, INPUT_PULLUP);
-  attachInterrupt(0, TachPulseInterrupt, FALLING);  
+  Serial.println("Setup");
+  Display_Setup();
+  Pt_Setup();
+  Fan_Setup();
 }
 
 void loop()
 { 
-  manageScreens();
-  manageFanSpeed();
-  managePT();
-  TachCalculateData();
+  Screen_Manage();
+  Fan_Manage();
+  Pt_Manage();
   delay(200);
 }
 
-void managePT()
+void Display_Setup()
 {
-  Serial.println("3");
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
-   
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure()/100.0);
-  Serial.println(" hPa");
-}
-
-void SetScreenCurrent(int screen, long duration)
-{
-  tm1637_6D.clearDisplay();
-  screenCurrent = screen;
-  millisChangeScreenTime = millis() + duration;
-
-  Serial.print(millis());
-  Serial.print(" Changed Screen to ");
-  Serial.print(screenCurrent);
-  Serial.print(" until ");
-  Serial.println(millisChangeScreenTime);
-}
-
-void manageScreens()
-{
-
-  if(millis() > millisChangeScreenTime)
+  Serial.println("Display_Setup");
+  tm1637_6D.init();
+  tm1637_6D.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  for(int i=0;i<6;i++)
   {
-    if(screenCurrent>=screenTopIndex){SetScreenCurrent(0, millisScreenToggleRate);}
-    else {SetScreenCurrent(++screenCurrent, millisScreenToggleRate);}
+    tm1637_6D.displayCodedByte(i,segTest);
   }
+   delay(200);
+   tm1637_6D.clearDisplay();
+}
 
-  switch(screenCurrent)
+void Pt_Setup()
+{
+  Serial.println("PtSetup Starting BME");
+  if (!Pt_Bme.begin(Pt_Address)) 
+  {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+  }
+  else
   {
-    case 0: // Setpoint Screen
-      showSetpointScreen();
+    Serial.println("BME Started");
+  }
+  
+  Pt_ZeroPressure();
+}
+
+void Pt_ZeroPressure()
+{
+  Serial.print("Zeroing PT: ");
+  Pt_ZeroHPa=Pt_Bme.readPressure()/100.0;
+  Pt_PressurePrint(Pt_ZeroHPa);
+  tm1637_6D.displayFloat(Pt_ZeroHPa);
+  delay(5000);
+}
+
+void Pt_PressurePrint(float hPa)
+{
+  Serial.print("Pressure = ");
+  Serial.print(hPa);
+  Serial.println(" hPa");  
+}
+
+void Pt_TemperaturePrint(float degC)
+{
+  Serial.print("Temperature = ");
+  Serial.print(degC);
+  Serial.println(" *C");
+}
+
+void Fan_Setup()
+{
+  Timer1.initialize(40);  // 40 us = 25 kHz
+
+  // Attach the tach interrupt
+  pinMode(pin_Tach, INPUT_PULLUP);
+  attachInterrupt(0, Fan_TachPulseInterrupt, FALLING);  
+}
+
+void Pt_Manage()
+{
+  static int loops = 0;
+  Pt_Hpa = Pt_Bme.readPressure()/100.0;
+  Pt_TempC = Pt_Bme.readTemperature();
+
+  loops++;
+
+  if (loops > 10)
+  {
+    Pt_TemperaturePrint(Pt_TempC);
+    Pt_PressurePrint(Pt_Hpa); 
+    loops = 0;
+  }  
+}
+
+void Screen_SetCurrent(int screen, long duration)
+{
+  if(screen != screen_Current) // Prevent flicker
+  {
+    tm1637_6D.clearDisplay();
+  }
+  screen_Current = screen;
+  screen_millisChangeScreenTime = millis() + duration;
+
+  /* Serial.print(millis());
+  Serial.print(" Changed Screen to ");
+  Serial.print(screen_Current);
+  Serial.print(" until ");
+  Serial.println(screen_millisChangeScreenTime);
+  */
+}
+
+void Screen_Manage()
+{
+  // Serial.println("ScreensManage");
+
+  if(millis() > screen_millisChangeScreenTime)
+  {
+    if(screen_Current>=screen_TopIndex){Screen_SetCurrent(0, screen_millisToggleRate);}
+    else {Screen_SetCurrent(++screen_Current, screen_millisToggleRate);}
+  }
+  switch(screen_Current)
+  {
+    case screen_SetpointLabel: // Setpoint Screen
+      ScreenRun_Label(segS);
       break;
-    case 1: // Actual Screen
-      showActualScreen();
+    case screen_Setpoint: // Setpoint Screen
+      ScreenRun_SetpointScreen();
       break;
-    case 2: // Relative Pressure Screen
-      showRelativePressureScreen();
+    case screen_ActualLabel: // Actual Screen
+      ScreenRun_Label(segA);
       break;
-    case 3: // Temperature Screen
-      showTemperatureScreen();
+    case screen_Actual: // Actual Screen
+      ScreenRun_ActualScreen();
       break;
-    case 4: // Pressure Zeroing Screen
-      showPressureZeroingScreen();
+    case screen_RelativePressureLabel: // Relative Pressure Screen
+      ScreenRun_Label(segP);
+      break;
+    case screen_RelativePressure: // Relative Pressure Screen
+      ScreenRun_RelativePressureScreen();
+      break;
+    case screen_TemperatureLabel: // Temperature Screen
+      ScreenRun_Label(segC);
+      break;    
+    case screen_Temperature: // Temperature Screen
+      ScreenRun_TemperatureScreen();
+      break;
+    case screen_PressureZeroingLabel: // Pressure Zeroing Screen
+       ScreenRun_Label(segTest);
+      break;
+    case screen_PressureZeroing: // Pressure Zeroing Screen
+      ScreenRun_PressureZeroingScreen();
       break;
     default: // Error Screen
-      showErrorScreen();
+      ScreenRun_ErrorScreen();
       break;
   }
-
 }
 
-int ReadPot()
+void Fan_Manage()
 {
-  return analogRead(Pin_Pot);
-}
+  static int last_fan_potInRaw = 0;
+  static int fan_ChangeRaw = 0;
 
-void manageFanSpeed()
-{
-  PotIn = ReadPot();
-  Timer1.pwm(Pin_PWMFan, PotIn);
-}
-
-void ZeroPressureTransmitter()
-{
-
-}
-
-void showSetpointScreen()
-{
-  Serial.println("showSetpointScreen");
-  tm1637_6D.displayByte(5,segS);
-}
-void showActualScreen()
-{
-  Serial.println("showActualScreen");
-  tm1637_6D.displayByte(5,segA);
-}
-void showRelativePressureScreen()
-{
-  Serial.println("showRelativePressureScreen");
-  tm1637_6D.displayByte(5,segP);
-}
-void showTemperatureScreen()
-{
-  Serial.println("showTemperatureScreen");
-  tm1637_6D.displayByte(5,segC);
-}
-void showPressureZeroingScreen()
-{
-  Serial.println("showPressureZeroingScreen");
-  tm1637_6D.displayInteger(123456,0);
+  Fan_TachCalculateData();
   
+  fan_potInRaw = analogRead(pin_Pot);
+  fan_SetpointIntPct = map(fan_potInRaw, 5, 1020, 0, 100);
+
+  // Calculate ABS change. Can't do math within the ABS function.
+  fan_ChangeRaw = last_fan_potInRaw - fan_potInRaw;
+  fan_ChangeRaw = abs(fan_ChangeRaw);
+      
+  if (fan_ChangeRaw > fan_SpeedChangeRawDelta)
+  {
+    Serial.print("Pot: ");
+    Serial.print(fan_potInRaw);
+    Serial.print("   -   Pct: ");
+    Serial.println(fan_SetpointIntPct);
+    Screen_SetCurrent(screen_Setpoint, fan_millisSpeedChangeFlagWindow);
+  }
+
+  last_fan_potInRaw = fan_potInRaw;
+  Timer1.pwm(pin_PWMFan, fan_potInRaw);
 }
-void showErrorScreen()
+
+void ScreenRun_Label(int8_t digitByte)
 {
-  Serial.println("showErrorScreen");
+  int8_t tempListDisp[6] = {digitByte,0,0,0,0,0}; // fill array with blank characters(10th)
+  tm1637_6D.displayCodedByte(tempListDisp);
+}
+void ScreenRun_SetpointScreen()
+{
+  // Serial.println("showSetpointScreen");
+  tm1637_6D.displayInteger(fan_SetpointIntPct,false);
+}
+void ScreenRun_ActualScreen()
+{
+  // Serial.println("showActualScreen");
+  tm1637_6D.displayInteger((int)fan_TachData.RPM,0);
+}
+void ScreenRun_RelativePressureScreen()
+{
+  // Serial.println("showRelativePressureScreen");
+  tm1637_6D.displayInteger(Pt_Hpa,0);
+}
+void ScreenRun_TemperatureScreen()
+{
+  // Serial.println("showTemperatureScreen");
+  tm1637_6D.displayFloat(Pt_TempC);
+}
+void ScreenRun_PressureZeroingScreen()
+{
+  // Serial.println("showPressureZeroingScreen");
+  tm1637_6D.displayFloat(Pt_ZeroHPa);
+}
+
+void ScreenRun_ErrorScreen()
+{
+  // Serial.println("showErrorScreen");
   tm1637_6D.displayError();
 }
 
-void TachPulseInterrupt(){
+void Fan_TachPulseInterrupt(){
   static int  pulses_missed = 0;
   
-  if(!interrupt_Lock){
-    pulsesCountedByInterrupt = pulsesCountedByInterrupt + pulses_missed + 1;
+  if(!fan_interrupt_Lock){
+    fan_pulsesCountedByInterrupt = fan_pulsesCountedByInterrupt + pulses_missed + 1;
     pulses_missed = 0;
-    microsCapturedByInterrupt = micros();
+    fan_microsCapturedByInterrupt = micros();
   }
   else {
     pulses_missed++;
   }
 }
 
-String TachDebugString(TachFeedbackData data){
+String Fan_TachDebugString(TachFeedbackData data){
   return String(data.pulses) + " pulses in " + String(data.millis_Duration, 4) + "ms, " + String(data.freq_Hz,2) + "Hz, " + String (data.RPM) + " RPM";
 }
 
-void TachCalculateData(){
+void Fan_TachCalculateData(){
     static unsigned long millis_next_run = 0;
 
     // Only calculate the rpm if we have exceeded the minimum interval
     if(millis() < millis_next_run) {return;}
 
     // We do not want to interfere with our interrupt here, so we exert a lock.
-    interrupt_Lock = true;
-    TachData.micros_Current = microsCapturedByInterrupt;
-    TachData.pulses = pulsesCountedByInterrupt;
-    pulsesCountedByInterrupt = 0;
-    interrupt_Lock = false;
+    fan_interrupt_Lock = true;
+    fan_TachData.micros_Current = fan_microsCapturedByInterrupt;
+    fan_TachData.pulses = fan_pulsesCountedByInterrupt;
+    fan_pulsesCountedByInterrupt = 0;
+    fan_interrupt_Lock = false;
 
-    millis_next_run = millis() + millis_TachCaptureInterval;
+    millis_next_run = millis() + fan_millis_TachCaptureInterval;
 
-    TachData.millis_Duration = (TachData.micros_Current-TachData.micros_Prev) / 1000.0;
+    fan_TachData.millis_Duration = (fan_TachData.micros_Current-fan_TachData.micros_Prev) / 1000.0;
     // There is a special case when the fan is stopped: Our pulses will be zero, and our interval can also be zero since it's updated in the
     // tach interrupt. Handle that case here by ignoring any data with a small duration.
-    if (TachData.millis_Duration < 50){
-        TachData.freq_Hz = 0;
-        TachData.RPM = 0;
+    if (fan_TachData.millis_Duration < 50){
+        fan_TachData.freq_Hz = 0;
+        fan_TachData.RPM = 0;
     }
     else {
-      TachData.freq_Hz = (TachData.pulses / TachData.millis_Duration) * 1000;
-      TachData.RPM = TachData.freq_Hz * 30; // (2 pulses per rev, so instead of Hz*60/2 just hardcode Hz*30)
+      fan_TachData.freq_Hz = (fan_TachData.pulses / fan_TachData.millis_Duration) * 1000;
+      fan_TachData.RPM = fan_TachData.freq_Hz * 30; // (2 pulses per rev, so instead of Hz*60/2 just hardcode Hz*30)
     }
     
-    TachData.micros_Prev = TachData.micros_Current;
-    Serial.println(TachDebugString(TachData));
+    fan_TachData.micros_Prev = fan_TachData.micros_Current;
+    Serial.println(Fan_TachDebugString(fan_TachData));
 }
+
 
